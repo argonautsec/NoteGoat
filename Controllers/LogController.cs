1f4cf9c225dbea70d7960acd37a763a85970c2bd
Harry Papaxenopoulos <harry@argonautsecurity.com>
2023-09-28 15:19:41 +0300

    Add vulnerable archive download for windows.

--- Controllers/LogController.cs
+++ Controllers/LogController.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Net.Mime;
 using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +11,12 @@ namespace NoteGoat.Controllers;
 [Authorize(Roles = Role.Admin)]
 public class LogController : Controller
 {
+    public struct BackupFile
+    {
+        public string Name;
+        public byte[] Content;
+    }
+
     private readonly ILogger<LogController> _logger;
     private readonly string _logFilePath;
 
@@ -22,5 +31,73 @@ public class LogController : Controller
         // TODO: Get last n lines instead
         ViewBag.Logs = System.IO.File.ReadAllText(_logFilePath, Encoding.UTF8);
         return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Download(string[] fileTypes)
+    {
+        if (fileTypes.Length == 0)
+        {
+            return NotFound();
+        }
+
+        BackupFile backup;
+        if (OperatingSystem.IsWindows())
+        {
+            backup = RunWindowsBackup(fileTypes);
+        }
+        else
+        {
+            backup = RunNixBackup(fileTypes);
+        }
+        return File(backup.Content, "application/octet-stream", backup.Name);
+    }
+
+    private BackupFile RunNixBackup(string[] fileTypes)
+    {
+        throw new NotImplementedException();
+    }
+
+    private BackupFile RunWindowsBackup(string[] fileTypes)
+    {
+        using var ps = PowerShell.Create();
+        ps.AddCommand("Compress-Archive");
+        foreach (var fileType in fileTypes)
+        {
+            ps.AddArgument(fileType);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append("Compress-Archive ");
+        stringBuilder.Append("-Force ");
+        stringBuilder.Append("-Path ");
+        for (int i = 0; i < fileTypes.Length; i++)
+        {
+            string? fileType = fileTypes[i];
+            stringBuilder.Append(fileType);
+            if (i < fileTypes.Length - 1)
+            {
+                stringBuilder.Append(',');
+            }
+        }
+        stringBuilder.Append(" -DestinationPath ");
+        stringBuilder.Append("Archive.zip");
+        _logger.LogInformation("Running {}", stringBuilder);
+        Process process = new();
+        process.StartInfo.FileName = "powershell";
+        process.StartInfo.Arguments = stringBuilder.ToString();
+        process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.Start();
+        process.WaitForExit();
+        string output = process.StandardOutput.ReadToEnd();
+        _logger.LogInformation(output);
+        string err = process.StandardError.ReadToEnd();
+        _logger.LogError(err);
+
+        var bytes = System.IO.File.ReadAllBytes("Archive.zip");
+        return new BackupFile { Name = "Archive.zip", Content = bytes };
     }
 }
